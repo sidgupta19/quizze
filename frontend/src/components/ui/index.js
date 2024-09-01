4a860f9b079d13775e93b053af5f9dee430d3cee
Siddharth Gupta <91104449+ded-abhishek@users.noreply.github.com>
2024-09-01 22:43:27 +0530

    www

--- frontend/src/components/ui/index.js
+++
@@ -1,5 +0,0 @@
-import Button from './Button';
-import FormInput from './FormInput';
-import Input from './Input'
-
-export { Button, FormInput, Input };
--- frontend/src/pages/User/Poll/PollResults.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './styles/PollResults.module.css';
-
-export default function PollResults() {
-  return (
-    <div className={styles.main}>Thank you for participating in the Poll</div>
-  );
-}
